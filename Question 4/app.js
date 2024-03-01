fc86cd07b864b1d881ee714ea30a71ddb2bad0cd
Awais_Ansari <mransari01234@gmail.com>
2024-03-01 07:38:30 +0500

    updated

---
+++ Question 4/app.js
@@ -0,0 +1,8 @@
+var userName = prompt("Enter Your Correct Name ")
+var myName = ("Owais") 
+
+if( userName == myName ){
+    console.log("Hi, ThankYou For Visting Our Website ")
+}else(
+    console.log()
+)
